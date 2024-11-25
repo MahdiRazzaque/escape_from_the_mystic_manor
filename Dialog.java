6b3f4fffa44fe810abf9dbdeb9858203732d6e4b
MahdiRazzaque <mahdi.razzaque+github@gmail.com>
2024-11-25 20:39:16 +0000

    Update Dialog.java
    Add cat dialog
    Removes the need to interact with each character multiple times

--- Dialog.java
+++ Dialog.java
@@ -10,7 +10,6 @@ import java.util.HashMap;
 public class Dialog {
 
     public static HashMap<String, ArrayList<String>> dialog = new HashMap<>(); // Stores the dialog for each character
-    public static HashMap<String, Integer> dialogNumber = new HashMap<>(); // Stores the current dialog number for each character
 
     // Static block to initialise the dialogs for the characters
     static {
@@ -20,7 +19,6 @@ public class Dialog {
         butlerDialog.add("The pantry key? It's well hidden, but perhaps you should seek the hidden chamber.");
         butlerDialog.add("Be careful, the ghost of the former owner is not easily defeated.");
         dialog.put("Butler", butlerDialog);
-        dialogNumber.put("Butler", 0);
 
         // Initialising dialog for Maid
         ArrayList<String> maidDialog = new ArrayList<>();
@@ -28,7 +26,6 @@ public class Dialog {
         maidDialog.add("The pantry is locked, but I have heard whispers about a key hidden in the hidden chamber.");
         maidDialog.add("If you're planning to face the ghost, you'll need something special. Have you spoken to the cat in the library?");
         dialog.put("Maid", maidDialog);
-        dialogNumber.put("Maid", 0);
 
         // Initialising dialog for Ghost of the Former Owner
         ArrayList<String> ghostDialog = new ArrayList<>();
@@ -36,15 +33,15 @@ public class Dialog {
         ghostDialog.add("To defeat me, you will need more than courage. Only a certain power can banish me.");
         ghostDialog.add("The hidden chamber holds many secrets. You will need to defeat me to access it.");
         dialog.put("Ghost of the Former Owner", ghostDialog);
-        dialogNumber.put("Ghost of the Former Owner", 0);
 
         // Initialising dialog for Cat
         ArrayList<String> catDialog = new ArrayList<>();
         catDialog.add("Meow. Welcome to the library. I have a riddle for you to solve.");
         catDialog.add("Solve my riddle and fetch me 5 coins, and I will give you the key weapon to defeat the ghost.");
         catDialog.add("I can remove dust and crumbs with ease, though I don’t use hands, water, or a breeze. What am I?");
+        catDialog.add("To solve the riddle, use the command 'answer [your answer]'. " +
+                "Ensure you possess no less than five coins to proceed.");
         dialog.put("Cat", catDialog);
-        dialogNumber.put("Cat", 0);
 
         // Initialising dialog for Security Guard
         ArrayList<String> securityGuardDialog = new ArrayList<>();
@@ -52,25 +49,25 @@ public class Dialog {
         securityGuardDialog.add("The ghost in the master bedroom is a formidable opponent. Make sure you're prepared.");
         securityGuardDialog.add("The cat in the library may have some valuable information for you.");
         dialog.put("Security Guard", securityGuardDialog);
-        dialogNumber.put("Security Guard", 0);
     }
 
     /**
-     * Retrieves and prints the next dialog for a given character.
+     * Retrieves and prints the next dialogue for a given character.
      * <p>
-     * This method retrieves the current dialog for the specified character and increments the dialog number.
-     * If the end of the dialog list is reached, it resets the dialog number to 0.
-     *
-     * @param character The name of the character whose dialog is to be retrieved.
+     * This method retrieves the current dialogue for the specified character and increments the dialogue number.
+     * If the end of the dialogue list is reached, it resets the dialogue number to 0.
+     * <p>
+     * @param character The name of the character whose dialogue is to be retrieved.
      */
     public static void getDialog(String character) {
         ArrayList<String> characterDialog = dialog.get(character);
-        Integer currentDialogNumber = dialogNumber.get(character);
-        if (currentDialogNumber == 2) {
-            dialogNumber.put(character, 0);
-        } else {
-            dialogNumber.put(character, currentDialogNumber + 1);
+
+        for (int i = 0; i < characterDialog.size(); i++) {
+            // Display the current dialogue line with numbering
+            System.out.println(String.format("[%d/%d] ", i + 1, characterDialog.size()) + characterDialog.get(i));
+
+            // Wait for 2 seconds before displaying the next line
+            Utils.waitSeconds(2);
         }
-        System.out.println(String.format("[%d/3] ", currentDialogNumber + 1) + characterDialog.get(currentDialogNumber));
     }
 }
